1bfade891aeb1379aa0917c583f60dff3581ecbf
Lethean2005 <lethean.seourn@student.passerellesnumeriques.org>
2025-07-06 23:48:23 +0700

    push code connection

--- BackEnd-laravel/routes/api.php
+++ BackEnd-laravel/routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,14 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// category
+Route::get('/categories', [CategoryController::class, 'index']);
+Route::get('/categories/{id}', [CategoryController::class, 'show']);
+Route::post('/categories', [CategoryController::class, 'store']);
+Route::put('/categories/{id}', [CategoryController::class, 'update']);
+Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
+
+// pproduct
+Route::apiResource('products', ProductController::class);
+Route::get('/categories/{id}/products', [CategoryController::class, 'products']);
